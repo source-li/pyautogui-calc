6a744dd09d65a48b0189f558653227ab2b0fef30
SOURCE LIGHT <source.light@yandex.ru>
2024-12-03 15:02:32 +0700

    Добавлен вывод результата в терминал

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import pyautogui
+import pyperclip
 import time
 
 
@@ -23,6 +24,17 @@ def click_button(image):
         pyautogui.click(pyautogui.center(button_location))
     else:
         print(f"Кнопка '{image}' не найдена на экране.")
+
+
+def get_calculator_result():
+    '''
+    Получаем результат из калькулятора с использованием горячих клавиш (Ctrl+C)
+    '''
+    pyautogui.hotkey('ctrl', 'c')
+    time.sleep(1)
+    
+    result = pyperclip.paste()
+    return result
     
 
 def main():
@@ -40,6 +52,8 @@ def main():
         click_button(f"data/{char}.png")
 
     click_button(f"data/=.png")
+    result = get_calculator_result()
+    print(f"Результат: {result}")
 
 if __name__ == "__main__":
     main()
